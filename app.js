bbcaa6c00eccea598065b6e38e65866a36a79510
campuzanogithub <128656913+campuzanogithub@users.noreply.github.com>
2024-02-29 21:04:26 -0500

    repaso web2 subir formulario

---
+++ app.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    console.log("Nombre: " + name);
+    console.log("Correo Electrónico: " + email);
+    console.log("Contraseña: " + password);
+}
+
+
